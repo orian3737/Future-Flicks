3af276499a13cd4e390b44a6ddebe9de7e5c82d2
Orian shillz <91493524+orian3737@users.noreply.github.com>
2024-06-19 21:54:45 -0400

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     <footer className="footer">
       <p>&copy; 2024 Future Flicks. All rights reserved.</p>
       <nav>
-        <a href="#home" onClick={() => window.scrollTo(0, 0)}>Home</a>
+        <a href="#app" onClick={() => window.scrollTo(0, 0)}>Home</a>
         <a href="#links" onClick={() => window.scrollTo(0, 0)}>Links</a>
         <h1>~ Thanks for Visiting ~</h1>
       </nav>
